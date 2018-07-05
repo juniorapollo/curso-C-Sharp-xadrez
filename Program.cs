423e9e0582af7f60ea4639383b62d58790703db9
juniorapollo <daniel.a.junior@hotmail.com>
2018-07-05 11:43:19 -0300

    Projeto Criado (POO)

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+
+
+namespace curso_C_Sharp_xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo X, Y;
+            X = new Triangulo();
+            Y = new Triangulo();
+
+            double areaX, areaY;
+
+            X.a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            areaX = X.calculaArea();
+            areaY = Y.calculaArea();
+
+            Console.WriteLine("AREA DO TRIANGULO X : " + areaX.ToString("F4"),CultureInfo.InvariantCulture);
+            Console.WriteLine("AREA DO TRIANGULO Y : " + areaY);
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("TRIANGULO DE MAIOR AREA:  X ");
+            }
+            else if (areaY > areaX)
+            {
+                Console.WriteLine("TRIANGULO DE MAIOR AREA:  Y ");
+            }
+            else
+            {
+                Console.WriteLine("AREAS IGUAIS ");
+
+            }
+
+
+
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# curso-c#-xadrez
+# curso-c-Sharp-xadrez
 Curso de C# , projeto xadrez.
+
+#Instalar SDK para rodar no MacOS e VSCode
+Criar pasta do projeto  
+
+#comandos 
+# 1- criar projeto 
+dotnet new console 
+# 2- Rodar
+dotnet run
+
+# namespace
+Forma para organizar os arquivos do projeto em módulos
+
+
+
